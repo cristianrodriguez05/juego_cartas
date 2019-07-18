6f5e2d92e2507aedc3371e2ecb0e47fff3bf83c2
jsmz <37253121+jsmzdf@users.noreply.github.com>
2019-07-17 22:28:32 -0500

    Add files via upload

---
+++ src/Cartas/Carta.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cartas;
+
+/**
+ *
+ * @author (╯°□°)╯︵ ┻━┻
+ */
+public class Carta {
+
+    public String imagen = "";
+    public String nombre = "";
+    public String clase = "";
+    public String tipo = "";
+    public String ataque = "";
+    public String ataquemagico = "";
+    public String defensa = "";
+    public String defensamagica = "";
+    public String vida = "";
+    public boolean buff = false;
+    public boolean debuff = false;
+    public String estado = "";
+    public int efectTurnos = 0;
+
+}
